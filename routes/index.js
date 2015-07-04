1afd7a2b4253ff7f489733dc50b7513b2ea4a7e5
venkat kotu <vikkii89@gmail.com>
2015-07-04 10:38:11 -0700

    Test api for blog

---
+++ routes/index.js
@@ -0,0 +1,93 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('./mysql');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/api/posts',function(req,res,next){
+  var qry = "select * from posts";
+  console.log("here2");
+  mysql.fetchData(qry,[],function(err,results){
+    if(!err){
+      console.log(results);
+      var posts = {
+        'posts' :results
+      };
+      res.send(posts);
+      res.end();
+    }else{
+      res.send({'error':"Error fetching the data"});
+      res.end();
+    }
+
+  });
+
+});
+
+
+router.get('/api/posts/:id',function(req,res,next) {
+  var qry = "select * from posts where id=?";
+  console.log("here");
+  var id = parseInt(req.params.id);
+  mysql.fetchData(qry, [id], function (err, results) {
+    if (!err) {
+      console.log(results);
+      var post = results[0];
+      res.send(post);
+      res.end();
+    } else {
+      res.send({'error': "Error fetching the data"});
+      res.end();
+    }
+
+  });
+
+});
+
+router.post('/api/posts/',function(req,res,next) {
+  var qry = "insert into posts (title,author) values (?,?)";
+
+  var post = req.body.post;
+  //console.log("here=> "+ title);
+  var title=post.title;
+  console.log(req.body);
+  var author = post.author;
+  mysql.execQuery(qry, [title,author], function (err, results) {
+    if (!err) {
+      res.send({'status':'success'});
+      res.end();
+    } else {
+      res.send({'status': "error"});
+      res.end();
+    }
+
+  });
+
+});
+
+router.put('/api/posts/',function(req,res,next) {
+  var qry = "update posts set author=?,title=? where id =?";
+
+  var post = req.body.post;
+  //console.log("here=> "+ title);
+  var title=post.title;
+  console.log(req.body);
+  var author = post.author;
+  var id = parseInt(post.id);
+  mysql.execQuery(qry, [author,title,id], function (err, results) {
+    if (!err) {
+      res.send({'status':'success'});
+      res.end();
+    } else {
+      res.send({'status': "error"});
+      res.end();
+    }
+
+  });
+
+});
+module.exports = router;
---
+++ routes/mysql.js
@@ -0,0 +1,52 @@
+/**
+ * Created by kotu on 7/3/15.
+ */
+var ejs= require('ejs');
+var mysql = require('mysql');
+
+function getConnection(){
+    var connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : 'password',
+        database : 'test'
+    });
+    return connection;
+}
+
+
+function fetchData(sqlQuery,params,callback){
+
+    console.log("\nSQL Query::"+sqlQuery);
+    var connection=getConnection();
+
+
+    connection.query(sqlQuery,params, function(err, rows, fields) {
+        if(err){
+            console.log("ERROR: " + err.message);
+        }
+        else
+        {	// return err or result
+            console.log("DB Results:"+rows);
+            callback(err, rows);
+        }
+    });
+    console.log("\nConnection closed..");
+    connection.end();
+}
+//params is an array
+function execQuery(query, params,callback) {
+    var connection=getConnection();
+
+    connection.query(query, params,function(err,result){
+        console.log("In exec Qeury = "+ err);
+        callback(err,result);
+    });
+
+    console.log("\nConnection closed..");
+    connection.end();
+
+}
+
+exports.fetchData=fetchData;
+exports.execQuery=execQuery;
